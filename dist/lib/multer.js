87beeaccd277ef617505a6f27fce12992ece8bd5
Hakimsabkhi <hakimsabkhi001@gmail.com>
2025-06-16 15:02:52 +0100

    chore: ignore dist build output

--- dist/lib/multer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.memoryUpload = void 0;
-// lib/multer.ts  – single instance for all image routes
-const multer_1 = __importDefault(require("multer"));
-exports.memoryUpload = (0, multer_1.default)({ storage: multer_1.default.memoryStorage() });
